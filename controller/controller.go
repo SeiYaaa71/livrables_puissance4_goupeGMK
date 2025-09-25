14d20129232b28df9913bc41089d6f51eba2fea9
matteo <matteo.bastide06@gmail.com>
2025-09-25 09:34:07 +0200

    [+]base

--- controller/controller.go
+++ controller/controller.go
@@ -9,3 +9,24 @@ func renderTemplate(w http.Respo,seWriter, filename string, data map{string}stri
 	tmpl := template.Must(template.ParseFiles("views/" + filename))
 	tmpl.Execute(w, data)
 }
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"Title": "Welcome to Connect Four",
+	}
+	renderTemplate(w, "home.html", data)	
+}
+
+func Play(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"Title": "Play Connect Four",
+	}
+	renderTemplate(w, "play.html", data)	
+}
+
+func Contact(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"Title": "Contact Us",
+	}
+	renderTemplate(w, "contact.html", data)	
+}
